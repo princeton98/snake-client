1f6ca015d6b3320bdb998065465cfc20702fb0d2
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-06 19:07:28 +0000

    added an exit function to the game

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const { connect } = require("./client");
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  handleUserInput(stdin);
+  stdin.resume();
+  return stdin;
+}
+const handleUserInput = function (stdin) {
+  stdin.on("data", (stdin) => {
+    if (stdin === '\u0003') {
+      process.exit();
+    }
+  }
+  )
+};
 console.log("Connecting ...");
 connect();
+setupInput();
+
+
 
